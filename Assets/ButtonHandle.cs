ece9a2f474656ef62f72f1de7526c05e52756e1b
Filipe Arthur <lipearthursilva@gmail.com>
2021-09-21 00:29:01 -0300

    Menu do jogo

---
+++ Assets/ButtonHandle.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System.Text;
+using System.IO;
+using UnityEngine.UI;
+
+public class ButtonHandle : MonoBehaviour
+{
+    //Read from a file
+    //string something = File.ReadAllText("C:\\Rfile.txt");
+    private string character;
+    public Image[] images;
+    public Text[] descricao;
+    public Button botao;
+
+    private void Start()
+    {
+        int c = 0;
+        while (c < images.Length && c < descricao.Length) {
+
+            images[c].gameObject.SetActive(false);
+            descricao[c].gameObject.SetActive(false);
+            c++;
+        }
+
+        if (botao) {
+
+            botao.gameObject.SetActive(false);
+        }
+
+        character = "Default";
+    }
+
+    public void selectChar(string personagem) {
+
+        int dont = -1;
+
+        switch (personagem) {
+
+            case "Ace": 
+                dont = 0;
+                break;
+            case "Anny":
+                dont = 1;
+                break;
+            case "Balthar":
+                dont = 2;
+                break;
+            case "Dion":
+                dont = 3;
+                break;
+            case "Viper":
+                dont = 4;
+                break;
+            case "Xaolin":
+                dont = 5;
+                break;
+        }
+
+        int c = 0;
+        while (c < images.Length && c < descricao.Length) {
+
+            images[c].gameObject.SetActive((c == dont));
+            descricao[c].gameObject.SetActive((c == dont));
+            c++;
+        }
+
+        if (botao)
+        {
+
+            botao.gameObject.SetActive(true);
+        }
+
+        character = personagem;
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        //Ace-SampleScene#1
+        /*int p1 = sceneName.IndexOf('-');
+        character = sceneName.Substring(0, p1);
+        string scene = sceneName.Substring(p1+1);*/
+        if (character != "Default")//NOT This function has been called only to change the scene
+        {
+            //Saving data
+            StreamWriter writer = new StreamWriter("SceneArgs.txt");
+            writer.Write(character);
+            writer.Close();
+        }
+        //
+        //Changing Scene
+        Application.LoadLevel(sceneName);
+    }
+}
