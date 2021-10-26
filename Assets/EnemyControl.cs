985f8f9956d19d808e6710fad16b9c281e095a9f
Marcos Ani Cury Vinagre Silva <65677865+MarcosAniCury@users.noreply.github.com>
2021-10-26 02:00:23 -0300

    Add simple waypoint scheme

---
+++ Assets/EnemyControl.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+
+
+public class EnemyControl : MonoBehaviour {
+
+	[SerializeField]
+	Transform[] waypoints;
+
+	[SerializeField]
+	Transform[] decisionWaypoints;
+
+	[SerializeField]
+	int[] decisionOption1;
+
+	
+	[SerializeField]
+	int[] decisionOption2;
+
+	
+	[SerializeField]
+	int[] decisionOption3;
+
+	[SerializeField]
+	float moveSpeed = 2f;
+
+	int waypointIndex = 0;
+
+	void Start () {
+		transform.position = waypoints [waypointIndex].transform.position;
+	}
+
+	void Update () {
+		Move ();
+	}
+
+	void Move()
+	{
+
+		foreach (Transform waypoint in decisionWaypoints) {
+			if (waypoint.position == transform.position) {
+				waypointIndex = choosePath();
+			}
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position,
+												waypoints[waypointIndex].transform.position,
+												moveSpeed * Time.deltaTime);
+
+		if (transform.position == waypoints [waypointIndex].transform.position) {
+			waypointIndex += 1;
+		}
+
+		if (waypointIndex == waypoints.Length) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	int choosePath() 
+	{
+		int decisionWaypointsIndex = 0;
+
+		for (int i = 0;i < decisionWaypoints.Length;i++){
+			if (transform.position == decisionWaypoints[i].position) {
+				decisionWaypointsIndex = i;
+				break;
+			}
+		}
+
+		int[] paths = { 
+						decisionOption1[decisionWaypointsIndex],
+						decisionOption2[decisionWaypointsIndex],
+						decisionOption3[decisionWaypointsIndex] 
+					};
+
+		int numberRandom = 0;
+		int numberChoose = 0;
+		while (numberChoose == 0) {
+			numberRandom = DateTime.Now.Millisecond % 3;
+			numberChoose = paths[numberRandom];
+		}
+		return numberChoose;
+	}
+
+}
